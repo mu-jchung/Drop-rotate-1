025567941e507067f7672bae419ac1c0e50c3cee
mu-jchung <chungjodad@gmail.com>
2015-10-14 00:16:53 -0400

    Drop-rotate-1: one texture -> one Sprite
    Single raindrop Sprite drawn at each Rectangle location, instead of single raindrop texture drawn at each Rectangle location.

--- desktop/src/com/badlogic/drop/desktop/DesktopLauncher.java
+++ desktop/src/com/badlogic/drop/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.badlogic.drop.Drop;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Drop";
+		config.title = "Drop rotation demo 1";
 		config.width = 800;
 		config.height = 480;
 		new LwjglApplication(new Drop(), config);
